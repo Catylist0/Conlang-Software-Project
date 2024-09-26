a6b0922d733303bf2656aa81adcde5baca3133a2
Catylist <henry.griffiths.dev@gmail.com>
2024-09-26 17:05:31 -0500

    added some silly functionality for learning

--- Langscaper/Views/MainWindow.axaml.cs
+++ Langscaper/Views/MainWindow.axaml.cs
@@ -1,11 +1,37 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace Langscaper.Views;
-
-public partial class MainWindow : Window
+namespace Langscaper.Views
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        // To track the current state
+        private bool isMovedLeft = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Click event handler
+        private void OnMoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Get the StackPanel by its name
+            var stackPanel = this.FindControl<StackPanel>("MainStackPanel");
+
+            if (isMovedLeft)
+            {
+                // Reset margin to move it back to the center
+                stackPanel.Margin = new Thickness(0);
+                isMovedLeft = false;
+            }
+            else
+            {
+                // Move the StackPanel to the left
+                stackPanel.Margin = new Thickness(-200, 0, 0, 0);
+                isMovedLeft = true;
+            }
+        }
     }
 }
